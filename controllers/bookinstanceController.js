65f2d7619a082e084c3759e6902c54207d8373bd
Luis <eternal_pitta0b@icloud.com>
2024-03-02 17:35:15 -0600

    handle book instances requests and render form

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,5 +1,7 @@
 const BookInstance = require("../models/bookinstance");
+const Book = require("../models/book");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator")
 
 // Display list of all BookInstances.
 exports.bookinstance_list = asyncHandler(async (req, res, next) => {
@@ -30,13 +32,65 @@ exports.bookinstance_detail = asyncHandler(async (req, res, next) => {
 
 // Display BookInstance create form on GET.
 exports.bookinstance_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance create GET");
+
+  const books = await Book.find({},'title').sort({title: 1}).exec();
+
+  res.render('bookinstance_form', {
+    title: "Create a Copy",
+    books,
+    selected_book: undefined,
+    bookInstance: undefined,
+    errors: []
+  })
 });
 
 // Handle BookInstance create on POST.
-exports.bookinstance_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance create POST");
-});
+exports.bookinstance_create_post = [
+  body('book', "Please select a book.")
+    .trim()
+    .isLength({min: 1})
+    .escape(),
+  body('imprint', "Imprint is required")
+    .trim()
+    .isLength({min: 1})
+    .withMessage("Imprint must be longer than 1 character.")
+    .escape(),
+  body("status")
+    .escape(),
+  body("due_back", "Invalid date")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+
+  asyncHandler(async (req, res, next) => {
+
+    const errors = validationResult(req);
+
+    const bookinstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back
+    });
+
+
+    if(!errors.isEmpty()){
+      const books = await Book.find({}, "title").sort({ title: 1 }).exec();
+
+      res.render('bookinstance_form', {
+        title: "Create a Copy",
+        books,
+        selected_book: bookinstance.book._id,
+        bookInstance: bookinstance,
+        errors: errors.array()
+      });
+      return;
+    } else {
+      await bookinstance.save();
+      res.redirect(bookinstance.url)
+    }
+  })
+];
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = asyncHandler(async (req, res, next) => {
