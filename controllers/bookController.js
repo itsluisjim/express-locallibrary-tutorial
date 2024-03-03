bdb0b91a14fff6ecde21ab397c27ba7c5831aa53
Luis <eternal_pitta0b@icloud.com>
2024-03-03 16:17:34 -0600

    add logic to delete books from db

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -5,7 +5,6 @@ const BookInstance = require("../models/bookinstance");
 const { body, validationResult } = require('express-validator');
 
 const asyncHandler = require("express-async-handler");
-const genre = require("../models/genre");
 
 exports.index = asyncHandler(async (req, res, next) => {
   // Get details of books, book instances, authors and genre counts (in parallel)
@@ -158,20 +157,126 @@ exports.book_create_post = [
 
 // Display book delete form on GET.
 exports.book_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book delete GET");
+  const book = await Book.findById(req.params.id).populate("author").exec();
+
+  if(book === null){
+    res.redirect("/catalog/books");
+  }
+
+  res.render('book_delete', {
+    title: "Delete book",
+    book
+  });
 });
 
 // Handle book delete on POST.
 exports.book_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book delete POST");
+  const book = await Book.findById(req.params.id).exec();
+
+  if (book === null) {
+    res.redirect("/catalog/books");
+    return;
+  } else {
+    await Book.findByIdAndDelete(req.body.bookid);
+    res.redirect("/catalog/books");
+  }
 });
 
 // Display book update form on GET.
 exports.book_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book update GET");
+  const [book, authors, genres] = await Promise.all([
+    Book.findById(req.params.id).populate("author").exec(),
+    Author.find().sort({family_name: 1}).exec(),
+    Genre.find().sort({name: 1}).exec()
+  ]);
+
+  if (book === null) {
+    const err = new Error("Book not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  genres.forEach((genre) => {
+    if (book.genre.includes(genre._id)) {
+     genre.checked = "true";
+    } 
+  });
+
+  res.render('book_form', {
+    title: "Update Book",
+    authors,
+    genres,
+    book: book,
+    errors: [],
+  })
 });
 
 // Handle book update on POST.
-exports.book_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book update POST");
-});
+exports.book_update_post = [
+  (req, res, next) => {
+    if (!Array.isArray(req.body.genre)) {
+      req.body.genre =
+        typeof req.body.genre === "undefined" ? [] : [req.body.genre];
+    }
+    next();
+  },
+
+  // Validate and sanitize fields.
+  body("title", "Title must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("author", "Author must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("summary", "Summary must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("isbn", "ISBN must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("genre.*")
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const book = new Book({
+      title: req.body.title,
+      author: req.body.author,
+      summary: req.body.summary,
+      isbn: req.body.isbn,
+      genre: typeof req.body.genre === "undefined" ? [] : req.body.genre,
+      _id: req.params.id, // This is required, or a new ID will be assigned!
+    });
+
+    if (!errors.isEmpty()) {
+      const [authors, genres] = await Promise.all([
+        Author.find().sort({ family_name: 1 }).exec(),
+        Genre.find().sort({ name: 1 }).exec(),
+      ]);
+
+      genres.forEach(genre => {
+        if (book.genre.indexOf(genre._id) > -1) {
+          genre.checked = "true";
+        }
+      });
+
+      res.render("book_form", {
+        title: "Update Book",
+        authors: authors,
+        genres: genres,
+        book: book,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      const updatedBook = await Book.findByIdAndUpdate(req.params.id, book, {});
+      res.redirect(updatedBook.url);
+    }
+  }),
+];
+
