b2713f113ed80f5f962aa82852c403cb63e42c4e
Luis <eternal_pitta0b@icloud.com>
2024-03-03 17:54:42 -0600

    add fuctionallity to update a genre

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -115,10 +115,52 @@ exports.genre_delete_post = asyncHandler(async (req, res, next) => {
 
 // Display Genre update form on GET.
 exports.genre_update_get = asyncHandler(async (req, res, next) => {
-  
+  const genre = await Genre.findById(req.params.id).exec();
+
+  if(genre === null){
+    res.redirect('/catalog/genres');
+    return;
+  } else {
+    res.render('genre_form', {
+      title: 'Update Genre',
+      genre,
+      errors: []
+    });
+  }
 });
 
 // Handle Genre update on POST.
-exports.genre_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre update POST");
-});
+exports.genre_update_post = [
+  body("name", "Genre name must contain at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const genre = new Genre({ 
+      _id: req.params.id,
+      name: req.body.name
+     });
+
+    if (!errors.isEmpty()) {
+      res.render("genre_form", {
+        title: "Update Genre",
+        genre: genre,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      const genreExists = await Genre.findOne({ name: req.body.name })
+      .collation({ locale: "en", strength: 2 })
+      .exec();
+      if (genreExists) {
+        res.redirect(genreExists.url);
+      } else {
+        const updatedGenre = await Genre.findByIdAndUpdate(req.params.id, genre, {});
+        res.redirect(updatedGenre.url);
+      }
+    }
+  }),
+];
